b49c96ddb897a24965008d0372761e8c664cae05
nirmal <a.m.s.nirmal.a@gmail.com>
2018-06-24 14:07:03 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { DataServiceService } from './serv/data-service.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hotel } from './models/hotel.model';
 
 
@@ -8,11 +8,14 @@ import { Hotel } from './models/hotel.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   hotels: Hotel[];
 
   constructor( private hotelService: DataServiceService ) {
+  }
+
+  ngOnInit() {
     this.hotelService.getHotelData()
       .subscribe( (data: Hotel[]) => {
         this.hotels = data;
